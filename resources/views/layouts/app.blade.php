d8e51e5a7a3b7de7161067ad5c00d45588317a18
elmaccho <106253529+elmaccho@users.noreply.github.com>
2023-12-21 19:07:15 +0100

    Navbar styles

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    {{-- Icons --}}
+    <script src="https://kit.fontawesome.com/4798a03daf.js" crossorigin="anonymous"></script>
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/nav.css')
+</head>
+<body>
+    <div id="app">
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,70 @@
+<nav class="navbar-wrapper">
+    <a class="navbar-brand brand-wrapper" href="{{ url('/') }}">
+        <img class="brand-logo" src="{{ asset('images/sharehub.png') }}" alt="">
+        <div class="brand-info">
+            <h1 class="brand-main">{{ config('app.name', 'Laravel') }}</h1>
+            <p class="brand-slogan">Share Inspire Connect</p>
+        </div>
+    </a>
+
+    @guest
+        @else
+    <div class="routes-wrapper col-4">
+        <a class="route-btn" href="#">
+            <i class="fa-solid fa-house-chimney"></i>
+        </a>
+        <a class="route-btn" href="#">
+            <i class="fa-solid fa-message"></i>
+        </a>
+        <a class="route-btn" href="#">
+            <i class="fa-solid fa-plus"></i>
+        </a>
+        <a class="route-btn" href="#">
+            <i class="fa-solid fa-bell"></i>
+        </a>
+        <a class="route-btn" href="#">
+            <i class="fa-solid fa-user"></i>
+        </a>
+        <a class="route-btn user-auth-route" href="#"  href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            <i class="fa-regular fa-user"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-end new-dropdown-menu mobiledropdown" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">
+                Profile
+            </a>
+
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </div>
+    </div>
+
+    <div class="auth-user">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle auth-user btn-main" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            <strong>{{ Auth::user()->name }}</strong>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-end new-dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">
+                Profile
+            </a>
+
+            <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </div>
+    </div> 
+    @endguest
+</nav>
