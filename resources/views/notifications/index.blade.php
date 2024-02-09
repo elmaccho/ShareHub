4acf48a78bf34a378df038cc57497132a142e7d2
elmaccho <106253529+elmaccho@users.noreply.github.com>
2024-02-09 11:03:16 +0100

    Notifications page restyle

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -1,18 +1,87 @@
 @extends('layouts.app')
 @section('title', 'ShareHub - Notifications')
 @section('content')
-<div class="container d-flex flex-column align-items-center gap-4">
-    <div class="col-10 d-flex flex-column align-items-center">
-        <h5>Notifications</h5>
-        <div class="d-flex gap-2">
-            <button class="btn btn-main btn-sm">All</button>
-            <button class="btn btn-main btn-sm">Friends request</button>
-            <button class="btn btn-main btn-sm">Chat</button>
-            <button class="btn btn-main btn-sm">Activity</button>
+<div class="container">
+    <h3 class="m-b-50 heading-line">Notifications <i class="fa fa-bell text-muted"></i></h3>
+
+    {{-- friend requests --}}
+    <div class="notification-ui_dd-content">
+        <div class="text-center mb-3">
+            You got a Friend Request!
+        </div>
+
+        <div class="notification-list notification-list--unread">
+            <div class="notification-list_content">
+                <div class="notification-list_img">
+                    @if (!is_null(Auth::user()->profile_image_path))
+                            <img src="{{ asset('storage/'. Auth::user()->profile_image_path) }}" alt="">
+                        @else
+                            <img src="{{ asset('storage/user_profile/userDefault.png') }}" alt="">
+                    @endif 
+                </div>
+                <div class="notification-list_detail">
+                        <p>
+                            <a class="text-decoration-none text-dark" href=""><strong>John Doe</strong></a> sends you a friend request
+                        </p>
+                            <small>10 mins ago</small>
+                        </p>
+                </div>
+            </div>
+            <div class="notification-list_feature-img">
+                <button class="btn btn-success btn-sm">Accept</button>
+                <button class="btn btn-danger btn-sm">Reject</button>
+            </div>
         </div>
     </div>
 
-    <div class="sh-section col-3" style="width: 800px;">ebez</div>
+    {{-- activity --}}
+    <div class="notification-ui_dd-content">
+        <div class="text-center mb-3">
+            Activity
+        </div>
+
+        <div class="notification-list notification-list--unread">
+            <div class="notification-list_content">
+                <div class="notification-list_img">
+                    @if (!is_null(Auth::user()->profile_image_path))
+                            <img src="{{ asset('storage/'. Auth::user()->profile_image_path) }}" alt="">
+                        @else
+                            <img src="{{ asset('storage/user_profile/userDefault.png') }}" alt="">
+                    @endif 
+                </div>
+                <div class="notification-list_detail">
+                    <p><a class="text-decoration-none text-dark" href=""><strong>John Doe</strong></a> reacted to your <a class="text-decoration-none" href=""><strong>post</strong></a></p>
+                    <p class="text-muted">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, dolorem.</p>
+                    <p class="text-muted"><small>10 mins ago</small></p>
+                </div>
+            </div>
+            <div class="notification-list_feature-img">
+            </div>
+        </div>
+        <div class="notification-list">
+            <div class="notification-list_content">
+                <div class="notification-list_img">
+                    @if (!is_null(Auth::user()->profile_image_path))
+                            <img src="{{ asset('storage/'. Auth::user()->profile_image_path) }}" alt="">
+                        @else
+                            <img src="{{ asset('storage/user_profile/userDefault.png') }}" alt="">
+                    @endif 
+                </div>
+                <div class="notification-list_detail">
+                    <p><a class="text-decoration-none text-dark" href=""><strong>Brian Cumin</strong></a> reacted to your <a class="text-decoration-none" href=""><strong>comment</strong></a></p>
+                    <p class="text-muted">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, dolorem.</p>
+                    <p class="text-muted"><small>10 mins ago</small></p>
+                </div>
+            </div>
+            <div class="notification-list_feature-img">
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center">
+        <a href="#!" class="dark-link">Load more activity</a>
+    </div>
 </div>
 @vite('resources/css/home.css')
+@vite('resources/css/notifications.css')
 @endsection
